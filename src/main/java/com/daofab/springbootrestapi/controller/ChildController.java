08ceb3848d0327172bef1ca7aa1bdcd549464354
JS Top1 Coder <jstopcoder0209@gmail.com>
2023-05-04 13:54:18 +0900

    Add controller for child

--- src/main/java/com/daofab/springbootrestapi/controller/ChildController.java
+++ src/main/java/com/daofab/springbootrestapi/controller/ChildController.java
@@ -1,10 +1,42 @@
 package com.daofab.springbootrestapi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.daofab.springbootrestapi.model.Child;
+import com.daofab.springbootrestapi.service.IChildService;
 
 @RestController
 @RequestMapping("/api/child")
 public class ChildController {
     
+    @Autowired
+    private IChildService childService;
+
+    @RequestMapping("/all")
+    public List<Child> findAll() {
+
+        List<Child> childs = childService.findAll();
+
+        return childs;
+    }
+
+    @RequestMapping("{id}")
+    public ResponseEntity<Child> findById(@PathVariable(value="id") int id) {
+        
+        Child child = null;
+
+        child = childService.findById(id);
+
+        if(child != null) {
+            return ResponseEntity.ok().body(child);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
